5f46f94400928d5a7bc0bbcee9987a6d2e70be38
TheLox95 <leo_9_t@hotmail.com>
2018-04-19 15:34:31 -0400

    work on button editor component;changed app title on banner

--- src/app/form-components/button/button.component.ts
+++ src/app/form-components/button/button.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  private css_classes: Map<string, string> = new Map();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get classes() {
+    return Array.from(this.css_classes.values()).join(' ');
+  }
+
+  setType(type) {
+    this.css_classes.set('type', type);
+  }
+
+  setSize(size) {
+    this.css_classes.set('size', size);
+  }
+
+  setEnabled(size) {
+    this.css_classes.set('enabled', size === true ? 'enabled' : 'disabled');
   }
 
 }
