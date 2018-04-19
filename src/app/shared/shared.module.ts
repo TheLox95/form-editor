9811538e2f4ceb175bd9a2e80b0366c4c69ba1d1
TheLox95 <leo_9_t@hotmail.com>
2018-04-19 13:10:40 -0400

    added bootstrap package

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule.forRoot()
   ],
-  declarations: []
+  declarations: [],
+  exports: [
+    CommonModule,
+    NgbModule
+  ]
 })
 export class SharedModule { }
